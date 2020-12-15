8a30fc736288afbe5072beb4f5304cd9199f7271
Stefan Frede <stefan@frede.dev>
2020-12-15 18:14:25 +0100

    feat: use built-in optimizations

--- snowpack.config.js
+++ snowpack.config.js
@@ -13,13 +13,6 @@ module.exports = {
         watch: '$1 --watch',
       },
     ],
-    [
-      '@snowpack/plugin-optimize',
-      {
-        preloadModules: true,
-        target: 'es2020',
-      },
-    ],
   ],
   installOptions: {
     NODE_ENV: true,
@@ -30,5 +23,12 @@ module.exports = {
   },
   devOptions: {
     open: 'none',
+  },
+  experiments: {
+    optimize: {
+      bundle: true,
+      minify: true,
+      target: 'es2020',
+    },
   },
 };
